5632514fc92217f8f2aa09d124e9ce6d64cdd4b9
wizard2014 <wizard2014@ya.ru>
2016-02-08 12:52:56 +0200

    code refactoring

--- Tests/Array2xmlTest.php
+++ Tests/Array2xmlTest.php
@@ -1,24 +1,27 @@
 <?php
-require_once '../array2xml.php';
 
-class Array2xmlTest extends PHPUnit_Framework_TestCase {
+use Utility\Array2xml;
 
+class Array2xmlTest extends PHPUnit_Framework_TestCase
+{
     /**
      * @var Array2xml
      */
     protected $array2xml;
 
-    protected function setUp(){
+    protected function setUp()
+    {
         $this->array2xml = new Array2xml();
         $this->array2xml->setNewTab("\t\t");
     }
 
-    protected function tearDown(){
+    protected function tearDown()
+    {
         $this->array2xml = null;
     }
 
-    protected function execute($expected_xml, $actual_array){
-
+    protected function execute($expected_xml, $actual_array)
+    {
         $actual_xml = $this->array2xml->convert($actual_array);
 
         $actual = new DOMDocument;
@@ -35,130 +38,145 @@ class Array2xmlTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($expected, $actual);
     }
 
-    public function testConvertDefault(){
-
-        $actual = array(
+    public function testConvertDefault()
+    {
+        $actual = [
             'item1' => 'Text 3',
             'item2' => 1234,
-            'item3' => array(
+            'item3' => [
                 'subItem1' => 'Text 1',
                 'subItem2' => 'Text 2'
-            ),
-        );
+            ],
+        ];
 
-        $expected = '<root>
-<item1>Text 3</item1>
-<item2>1234</item2>
-<item3>
-        <subItem1>Text 1</subItem1>
-        <subItem2>Text 2</subItem2>
-</item3>
-</root>';
+        $expected = '
+                    <root>
+                        <item1>Text 3</item1>
+                        <item2>1234</item2>
+                        <item3>
+                            <subItem1>Text 1</subItem1>
+                            <subItem2>Text 2</subItem2>
+                        </item3>
+                    </root>
+                    ';
 
         $this->execute($expected, $actual);
     }
 
-    public function testSkipNumeric(){
-        $actual = array(
+    public function testSkipNumeric()
+    {
+        $actual = [
             '1234' => 'Text 3',
             'item2' => 1234,
-            'item3' => array(
+            'item3' => [
                 'subItem1' => 'Text 1',
                 'subItem2' => 'Text 2'
-            ),
-        );
+            ],
+        ];
 
-        $expected = '<root>
-<item2>1234</item2>
-<item3>
-        <subItem1>Text 1</subItem1>
-        <subItem2>Text 2</subItem2>
-</item3>
-</root>';
+        $expected = '
+                    <root>
+                        <item2>1234</item2>
+                        <item3>
+                            <subItem1>Text 1</subItem1>
+                            <subItem2>Text 2</subItem2>
+                        </item3>
+                    </root>
+                    ';
 
         $this->execute($expected, $actual);
     }
 
-    public function testNoSkipNumeric(){
-        $actual = array(
+    public function testNoSkipNumeric()
+    {
+        $actual = [
             '1234' => 'Text 3',
             'item2' => 1234,
-            'item3' => array(
+            'item3' => [
                 'subItem1' => 'Text 1',
                 'subItem2' => 'Text 2'
-            ),
-        );
+            ],
+        ];
 
-        $expected = '<root>
-<key1234>Text 3</key1234>
-<item2>1234</item2>
-<item3>
-        <subItem1>Text 1</subItem1>
-        <subItem2>Text 2</subItem2>
-</item3>
-</root>';
+        $expected = '
+                    <root>
+                        <key1234>Text 3</key1234>
+                        <item2>1234</item2>
+                        <item3>
+                            <subItem1>Text 1</subItem1>
+                            <subItem2>Text 2</subItem2>
+                        </item3>
+                    </root>
+                    ';
 
-        $this->array2xml->setSkipNumeric(FALSE);
+        $this->array2xml->setSkipNumeric(false);
 
         $this->execute($expected, $actual);
     }
 
-    public function testFilterNumbersAll(){
-        $actual = array(
+    public function testFilterNumbersAll()
+    {
+        $actual = [
             'item1' => 'Text 3',
             'item2' => 1234,
-            'item3' => array(
+            'item3' => [
                 'subItem1' => 'Text 1',
                 'subItem2' => 'Text 2'
-            ),
-        );
+            ],
+        ];
 
-        $expected = '<root>
-<item>Text 3</item>
-<item>1234</item>
-<item>
-        <subItem>Text 1</subItem>
-        <subItem>Text 2</subItem>
-</item>
-</root>';
+        $expected = '
+                    <root>
+                        <item>Text 3</item>
+                        <item>1234</item>
+                        <item>
+                            <subItem>Text 1</subItem>
+                            <subItem>Text 2</subItem>
+                        </item>
+                    </root>
+                    ';
 
-        $this->array2xml->setFilterNumbersInTags(TRUE);
+        $this->array2xml->setFilterNumbersInTags(true);
 
         $this->execute($expected, $actual);
     }
 
-    public function testFilterNumbersSpecific(){
-        $actual = array(
+    public function testFilterNumbersSpecific()
+    {
+        $actual = [
             'foo1' => 'Text 3',
             'bar1' => 1234,
-            'baz1' => array(
+            'baz1' => [
                 'subBaz1'   => 'Text 1',
                 'subBaz2'   => 'Text 1',
                 'subBaz3'   => 'Text 1',
                 'subBaz4'   => 'Text 1',
-                'subItem2' => 'Text 2'
-            ),
-        );
+                'subItem2'  => 'Text 2'
+            ],
+        ];
 
-        $expected = '<root>
-<foo>Text 3</foo>
-<bar1>1234</bar1>
-<baz1>
-        <subBaz>Text 1</subBaz>
-        <subBaz>Text 1</subBaz>
-        <subBaz>Text 1</subBaz>
-        <subBaz>Text 1</subBaz>
-        <subItem2>Text 2</subItem2>
-</baz1>
-</root>';
+        $expected = '
+                    <root>
+                        <foo>Text 3</foo>
+                        <bar1>1234</bar1>
+                        <baz1>
+                            <subBaz>Text 1</subBaz>
+                            <subBaz>Text 1</subBaz>
+                            <subBaz>Text 1</subBaz>
+                            <subBaz>Text 1</subBaz>
+                            <subItem2>Text 2</subItem2>
+                        </baz1>
+                    </root>
+                    ';
 
-        $this->array2xml->setFilterNumbersInTags(array('foo','subBaz'));
+        $this->array2xml->setFilterNumbersInTags(['foo', 'subBaz']);
 
         $this->execute($expected, $actual);
     }
 
-    public function testEmptyElementSyntaxFull(){
-        $actual = array('foo' => '', 'bar' => 'text');
+    public function testEmptyElementSyntaxFull()
+    {
+        $actual = ['foo' => '', 'bar' => 'text'];
         $expected = '<root><foo></foo><bar>text</bar></root>';
 
         $this->array2xml->setEmptyElementSyntax(Array2xml::EMPTY_FULL);
@@ -166,8 +184,9 @@ class Array2xmlTest extends PHPUnit_Framework_TestCase {
         $this->execute($expected, $actual);
     }
 
-    public function testConvertEmptyElementSyntaxSelf(){
-        $actual = array('foo' => '', 'bar' => 'text');
+    public function testConvertEmptyElementSyntaxSelf()
+    {
+        $actual = ['foo' => '', 'bar' => 'text'];
         $expected = '<root><foo/><bar>text</bar></root>';
 
         $this->array2xml->setEmptyElementSyntax(Array2xml::EMPTY_SELF_CLOSING);
@@ -175,37 +194,42 @@ class Array2xmlTest extends PHPUnit_Framework_TestCase {
         $this->execute($expected, $actual);
     }
 
-    public function testElementAttrsDefault(){
-        $actual = array('foo' => 'Some', 'bar' => 'Text');
+    public function testElementAttrsDefault()
+    {
+        $actual = ['foo' => 'Some', 'bar' => 'Text'];
         $expected = '<root><foo test="true">Some</foo><bar>Text</bar></root>';
 
-        $this->array2xml->setElementsAttrs(array('foo' => array('test' => 'true')));
+        $this->array2xml->setElementsAttrs(['foo' => ['test' => 'true']]);
 
         $this->execute($expected, $actual);
     }
 
-    public function testElementAttrsDynamic(){
-        $actual = array(
-            'foo' => array('@content' => 'Some', '@attributes' => array('test' => 'true')),
+    public function testElementAttrsDynamic()
+    {
+        $actual = [
+            'foo' => ['@content' => 'Some', '@attributes' => ['test' => 'true']],
             'bar' => 'Text'
-        );
+        ];
+
         $expected = '<root><foo test="true">Some</foo><bar>Text</bar></root>';
 
         $this->execute($expected, $actual);
     }
 
-    public function testRootAttrs(){
+    public function testRootAttrs()
+    {
 
-        $actual = array('foo' => 'text');
+        $actual = ['foo' => 'text'];
         $expected = '<root someAttr="value" someOtherAttr="some value"><foo>text</foo></root>';
 
-        $this->array2xml->setRootAttrs(array('someAttr'=>'value', 'someOtherAttr' => 'some value'));
+        $this->array2xml->setRootAttrs(['someAttr' => 'value', 'someOtherAttr' => 'some value']);
 
         $this->execute($expected,$actual);
     }
 
-    public function testRootName(){
-        $actual = array('foo' => 'text');
+    public function testRootName()
+    {
+        $actual = ['foo' => 'text'];
         $expected = '<products><foo>text</foo></products>';
 
         $this->array2xml->setRootName('products');
@@ -213,8 +237,9 @@ class Array2xmlTest extends PHPUnit_Framework_TestCase {
         $this->execute($expected,$actual);
     }
 
-    public function testNonAssocArrayDefault(){
-        $actual = array(1,5,2,'some', null);
+    public function testNonAssocArrayDefault()
+    {
+        $actual = [1, 5, 2, 'some', null];
 
         $expected1_xml = '<root></root>';
 
@@ -233,35 +258,40 @@ class Array2xmlTest extends PHPUnit_Framework_TestCase {
         );
     }
 
-    public function testNonAssocArrayConfigured(){
+    public function testNonAssocArrayConfigured()
+    {
+        $actual = [1, 5, 2, 'some', null];
 
-        $actual = array(1,5,2,'some', null);
-        $expected_xml = '<root>
-<key0>1</key0>
-<key1>5</key1>
-<key2>2</key2>
-<key3>some</key3>
-<key4></key4>
-</root>';
+        $expected_xml = '
+                        <root>
+                            <key0>1</key0>
+                            <key1>5</key1>
+                            <key2>2</key2>
+                            <key3>some</key3>
+                            <key4></key4>
+                        </root>
+                        ';
 
         $this->array2xml->setEmptyElementSyntax(Array2xml::EMPTY_FULL);
-        $this->array2xml->setSkipNumeric(FALSE);
+        $this->array2xml->setSkipNumeric(false);
 
         $this->execute($expected_xml, $actual);
     }
 
-    public function testCyrillic(){
-        $actual = array('itemOne'=>'Один', 'itemTwo' => 'Демо строка');
-        $expected_xml = '<root><itemOne>Один</itemOne><itemTwo>Демо строка</itemTwo></root>';
+    public function testCyrillic()
+    {
+        $actual = ['itemOne' =>'One', 'itemTwo' => 'Demo string'];
+        $expected_xml = '<root><itemOne>One</itemOne><itemTwo>Demo string</itemTwo></root>';
 
         $this->execute($expected_xml, $actual);
     }
 
-    public function testInvalidAttributesDynamic(){
-        $actual = array(
-            'foo' => array('@content' => 'Some'),
+    public function testInvalidAttributesDynamic()
+    {
+        $actual = [
+            'foo' => ['@content' => 'Some'],
             'bar' => 'Text'
-        );
+        ];
 
         $expected_xml = '<root><foo></foo><bar>Text</bar></root>';
 
@@ -279,9 +309,4 @@ class Array2xmlTest extends PHPUnit_Framework_TestCase {
             $expected->firstChild, $actual->firstChild, true
         );
     }
-
-
-
-
-
 }
